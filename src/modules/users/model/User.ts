2be31961a9d2019e776fedad25bdb8eaed9e9311
RafaelCopes <rafaelcopes16@gmail.com>
2021-12-21 17:20:14 -0300

    uploading challenge to github

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,25 @@
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuid } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+
+  name: string;
+
+  admin: boolean;
+
+  email: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    this.admin = false;
+
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
 
 export { User };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("This user does not exists!");
+    }
+
+    if (!user.admin) {
+      throw new Error("This user is not an admin!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
